bd92244f93f8a9015f466f4df2cd04a71cccb860
Ashley Alfred <ashley@sagerecruiting.me>
2020-10-24 16:42:06 -0400

    Added contact email support from Mailgun

--- application.py
+++ application.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template, request
-import sqlite3
-
-# import smtplib
-# from email.message import EmailMessage
-
+import requests
+import os
 
 app = Flask(__name__)
+
+def send_email(name, email, subject, message):
+    mailgun_domain = os.environ.get('MAILGUN_DOMAIN')
+    return requests.post(
+        f'https://api.mailgun.net/v3/{mailgun_domain}/messages',
+        auth=("api", os.environ.get('MAILGUN_API_KEY')),
+        data={
+            'from': 'Homepage <mailgun@sandbox.mgsend.net>',
+            'to': [os.environ.get('CONTACT_EMAIL_ADDR')],
+            'subject': subject,
+            'text': f'From: {name} <{email}>\n\n{message}'
+        }
+    )
+
+def save_contact(name, email, subject, message):
+    import sqlite3
+    conn = sqlite3.connect('website.db')
+    c = conn.cursor() 
+    c.execute("INSERT into registrants (name, email, subject, message) VALUES (?, ?, ?, ?)", (name, email, subject, message))
+    conn.commit()
+    conn.close()
 
 @app.route("/")
 def index():
@@ -32,20 +50,8 @@ def register():
         email = request.form.get("email")
         subject = request.form.get("subject")
         message = request.form.get("message")
-        conn = sqlite3.connect('website.db')
-        c = conn.cursor() 
-        c.execute("INSERT into registrants (name, email, subject, message) VALUES (?, ?, ?, ?)", (name, email, subject, message))
-        conn.commit()
-        conn.close()
-        # msg = EmailMessage()
-        # msg.set_content(message)
-        # msg['Subject'] = subject
-        # msg['From'] = "ashley@sagerecruiting.me"
-        # msg['To'] = "ashley@sagerecruiting.me"
-        # # Send the message via our own SMTP server.
-        # s = smtplib.SMTP('localhost')
-        # s.send_message(msg)
-        # s.quit()
+        # save_contact(name, email, subject, message)
+        send_email(name, email, subject, message)
         return render_template("response.html", name=name, email=email, subject=subject, message=message)
 
 if __name__ == '__main__':
